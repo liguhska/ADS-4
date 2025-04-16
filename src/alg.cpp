6c808e3cc5316d35315c7c985a3dee87615e8321
liguhska <masharostenko@mail.ru>
2025-04-16 18:26:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,27 +12,24 @@ int countPairs1(int* arr, int len, int value) {
 }
 
 int countPairs2(int* arr, int len, int value) {
-  int count = 0;
-  int left = 0;
-  int right = len - 1;
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
 
-  while (left < right) {
-    int sum = arr[left] + arr[right];
-    if (sum == value) {
-      count++;
-      left++;
-      right--;
-      while (left < right && arr[left] == arr[left - 1])
+    while (arr[left] + arr[right] < value && left != right) {
         left++;
-      while (left < right && arr[right] == arr[right + 1])
-        right--;
-    } else if (sum < value) {
-      left++;
-    } else {
-      right--;
     }
-  }
-  return count;
+    while (arr[right] > value && right != left) {
+        right--;
+    }
+    for (int i = left; i < right; i++) {
+        for (int j = i + 1; j <= right; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
 
 int countPairs3(int* arr, int len, int value) {
