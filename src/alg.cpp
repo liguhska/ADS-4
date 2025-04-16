2afc0067e0a141f867918a41a1af584b1823f380
liguhska <masharostenko@mail.ru>
2025-04-16 08:23:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,9 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int* arr, int len, int value)
-{
+int countPairs1(int* arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len; i++)
-  {
-    for (int j = 0; j < len; j++)
-    {
-      if (arr[i] + arr[j] == value)
-      {
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
         count++;
       }
     }
@@ -15,14 +11,12 @@ int countPairs1(int* arr, int len, int value)
   return count;
 }
 
-int countPairs2(int* arr, int len, int value)
-{
+int countPairs2(int* arr, int len, int value) {
   int count = 0;
   int left = 0;
   int right = len - 1;
 
-  while (left < right)
-  {
+  while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
       count++;
@@ -33,22 +27,18 @@ int countPairs2(int* arr, int len, int value)
       while (left < right && arr[right] == arr[right + 1])
         right--;
     }
-    else if (sum < value)
-    {
+    else if (sum < value) {
       left++;
     }
-    else
-    {
+    else {
       right--;
     }
   }
   return count;
 }
 
-int binarySearch(int* arr, int left, int right, int x)
-{
-  while (left <= right)
-  {
+int binarySearch(int* arr, int left, int right, int x) {
+  while (left <= right) {
     int middle = (left + right) / 2;
 		
     if (arr[middle] == x)
@@ -61,22 +51,20 @@ int binarySearch(int* arr, int left, int right, int x)
   return -1;
 }
 
-int countPairs3(int* arr, int len, int value)
-{
+int countPairs3(int* arr, int len, int value) {
   int count = 0;
 
-  for (int i = 0; i < len - 1; i++)
-  {
+  for (int i = 0; i < len - 1; i++) {
     int result = value - arr[i];
     int index = binarySearch(arr, i + 1, len - 1, result);
-    if (index != -1)
-    {
+    while (index != -1) {
       count++;
-      while (index + 1 < len && arr[index] == arr[index + 1])
-      {
+      index++;
+      while (index < len && arr[index] == arr[index + 1]) {
         count++;
         index++;
       }
+      break;
     }
   }
   return count;
